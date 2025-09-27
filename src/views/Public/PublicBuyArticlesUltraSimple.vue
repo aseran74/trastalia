00bc8f28630ba719061c8081bda001801e8ad4f4
aseran74 <alvaroserr@gmail.com>
2025-09-27 21:47:52 +0200

    style: Mejorar diseño del menú móvil con márgenes y bordes redondeados
    - Agregar márgenes laterales (mx-4) al menú móvil
    - Añadir padding interno (py-6 px-6) para mejor espaciado
    - Incluir bordes redondeados inferiores (rounded-b-xl)
    - Aumentar espaciado entre elementos (space-y-5)
    - Aplicar mejoras tanto en LandingPage como en PublicBuyArticlesUltraSimple
    - Mejorar aspecto visual y UX del menú desplegable móvil

--- src/views/Public/PublicBuyArticlesUltraSimple.vue
+++ src/views/Public/PublicBuyArticlesUltraSimple.vue
@@ -47,8 +47,8 @@
           </div>
 
           <!-- Mobile Menu -->
-          <div v-if="mobileMenuOpen" class="md:hidden mt-4 py-4 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 relative z-50 shadow-lg">
-            <div class="flex flex-col space-y-4">
+          <div v-if="mobileMenuOpen" class="md:hidden mt-4 mx-4 py-6 px-6 border-t border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 relative z-50 shadow-lg rounded-b-xl">
+            <div class="flex flex-col space-y-5">
               
               <router-link to="/" class="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white transition-colors" @click="mobileMenuOpen = false">Inicio</router-link>
               <a href="#categorias" class="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white transition-colors" @click="mobileMenuOpen = false">Categorías</a>
